19d4dee5c05b2d4a7ec8a35f794687022199dcd6
Jefferson Felix <me@jeffersonfelix.dev>
2024-08-05 23:41:37 -0300

    fix: env file and package lock file

--- packages/env/env.ts
+++ packages/env/env.ts
@@ -5,16 +5,23 @@ import { getRuntimeVariable } from './getRuntimeVariable'
 
 export const env = createEnv({
   server: {
+    NODE_ENV: z
+      .enum(['development', 'test', 'production'])
+      .default('development'),
+    APP_PORT: z.coerce.number().default(3333),
+    APP_SWAGGER_USER: z.string().default('dev'),
+    APP_SWAGGER_PASSWORD: z.string().default('dev'),
+    APP_JWK_PRIVATE_KEY: z.string(),
     DATABASE_URL: z
       .string()
       .url()
       .refine((url) => url.startsWith('postgresql://')),
-    APP_PORT: z.coerce.number().default(3333),
   },
-  client: {},
-  runtimeEnv: {
-    DATABASE_URL: getRuntimeVariable('DATABASE_URL'),
-    APP_PORT: getRuntimeVariable('APP_PORT'),
+  client: { NEXT_PUBLIC_APP_JWK_PUBLIC_KEY: z.string().default('{}') },
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_APP_JWK_PUBLIC_KEY: getRuntimeVariable(
+      'NEXT_PUBLIC_APP_JWK_PUBLIC_KEY',
+    ),
   },
   onValidationError(error) {
     console.error('❌ Invalid environment variables! See errors bellow:')
